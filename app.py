9237f61c2070da9e7a300b1bed5d35c89b4aae1f
emaapriliana <rahmatina.ari@gmail.com>
2023-07-03 10:18:01 +0700

    update pattern RegEx

--- app.py
+++ app.py
@@ -42,15 +42,18 @@ def lowercase(s):
     return s.lower()
 
 def remove_punctuation(s):
-    s = re.sub('[^0-9a-zA-Z]+', ' ', s)
-    s = re.sub(r':', '', s)
-    s = re.sub('\n',' ',s)
-    s = re.sub('rt',' ', s)
-    s = re.sub('((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))',' ', s)
-    s = re.sub('  +', ' ', s)
-    s = re.sub(r'pic.twitter.com.[\w]+', '', s)
-    s = re.sub('user',' ', s)
-    s = re.sub('gue','saya', s)
+    s = re.sub('((www\.[^\s]+)|(https?:\/\/[^\s]+)|(http?:\/\/[^\s]+)|(http[^\s]+))',' ', s) #menghapus semua URL
+    s = re.sub(r'(?:\\x[A-Fa-f0-9]{2})+', r'', s) #menghapus \xDD substring
+    s = re.sub('[^0-9a-zA-Z]+', ' ', s) #menghilangkan semua karakter yang bukan huruf atau angka dan menggantinya dengan spasi.
+    s = re.sub(r'\brt\b',' ', s) #menghapus awalan rt
+    s = re.sub('gue','saya', s) # Mengganti kata "gue" dengan kata "saya"
+    s = re.sub(r'\d+', '', s) #menghapus semua angka
+    s = re.sub(r'\buser\b',' ', s) #menghapus kata 'user'
+    s = re.sub(r':', ' ', s) #menggantikan karakter : dengan spasi 
+    s = re.sub(' +', ' ', s) #menggantikan satu atau lebih spasi berturut-turut dengan satu spasi 
+    s = re.sub('\n',' ',s) #menggantikan karakter newline (\n) dengan spasi 
+    s = re.sub(r'pic.twitter.com.[\w]+', ' ', s) #menghapus semua tautan Twitter (pic.twitter.com)
+    s = s.strip() #menghilangkan whitespace di awal dan di akhir teks
     s = re.sub(r'‚Ä¶', '', s)
     return s
 
